be2956d169f09dc5380eb2f0a37ecd20c34bb591
qbentil <bentilshadrack72@gmail.com>
2022-06-04 01:45:56 +0000

    Intialize Cart Screen

---
+++ screens/Cart/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+interface Props {
+  navigation: any
+}
+const CartScreen:React.FC<Props> = ({navigation}) => {
+  return (
+    <div>CartScreen</div>
+  )
+}
+
+export default CartScreen
