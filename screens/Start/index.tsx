d50a7a0898ebf94078ab810febd52c484823996f
qbentil <bentilshadrack72@gmail.com>
2022-06-04 01:46:22 +0000

    Initialize Start Screen

---
+++ screens/Start/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface Props {
+  navigation: any
+}
+
+const StartScreen:React.FC<Props> = ({navigation}) => {
+  return (
+    <div>StartScreen</div>
+  )
+}
+
+
+export default StartScreen
