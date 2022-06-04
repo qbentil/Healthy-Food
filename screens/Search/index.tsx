aeace654340c7219dfc7a7ed991edbabea1473ac
qbentil <bentilshadrack72@gmail.com>
2022-06-04 01:53:19 +0000

    Initialize Search Screen

---
+++ screens/Search/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+interface Props{
+    navigation: any
+}
+const SearchScreen:React.FC<Props> = ({navigation}) => {
+  return (
+    <div>SearchScreen</div>
+  )
+}
+
+export default SearchScreen
