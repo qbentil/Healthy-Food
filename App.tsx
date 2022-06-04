9739031766bc86f9e45355010c9a1e132f525978
qbentil <bentilshadrack72@gmail.com>
2022-06-04 01:53:47 +0000

    Update Routes ✔

--- App.tsx
+++ App.tsx
@@ -1,15 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native';
-
+import CartScreen from './screens/Cart';
 import HomeScreen from './screens/Home';
+import SearchScreen from './screens/Search';
 import StartScreen from './screens/Start';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
       <Stack.Navigator initialRouteName='Start'>
-        <Stack.Screen name="Start" component={StartScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Start" component={StartScreen}  options = {{headerShown: false}}/>
+        <Stack.Screen name="Home" component={HomeScreen} options = {{headerShown: false}}/>
+        <Stack.Screen name="Cart" component={CartScreen} options = {{headerShown: false}}/>
+        <Stack.Screen name="Search" component={SearchScreen} options = {{headerShown: false}}/>
       </Stack.Navigator>
   );
 }
