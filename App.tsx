506bd64bc09e430f8efe84b0324b0852417c5432
qbentil <bentilshadrack72@gmail.com>
2022-06-04 01:32:41 +0000

    Check for Tailwind Config

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import { StatusBar } from 'expo-status-bar';
+import tw from 'twrnc';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={tw`text-red-600`}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
