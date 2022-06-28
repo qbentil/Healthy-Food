09a5e8de8b2d05b923ee260f6da9ba21adf45b0b
qbentil <bentilshadrack72@gmail.com>
2022-06-28 17:21:22 +0000

    Update Root

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
+import { SafeAreaView, Text, View } from 'react-native';
+
 import CartScreen from './screens/Cart';
 import HomeScreen from './screens/Home';
+import {NavigationContainer} from '@react-navigation/native';
 import SearchScreen from './screens/Search';
 import StartScreen from './screens/Start';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -8,12 +11,14 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-      <Stack.Navigator initialRouteName='Start'>
-        <Stack.Screen name="Start" component={StartScreen}  options = {{headerShown: false}}/>
-        <Stack.Screen name="Home" component={HomeScreen} options = {{headerShown: false}}/>
-        <Stack.Screen name="Cart" component={CartScreen} options = {{headerShown: false}}/>
-        <Stack.Screen name="Search" component={SearchScreen} options = {{headerShown: false}}/>
-      </Stack.Navigator>
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName='Start'>
+    <Stack.Screen name="Start" component={StartScreen}  options = {{headerShown: false}}/>
+    <Stack.Screen name="Home" component={HomeScreen} options = {{headerShown: false}}/>
+    <Stack.Screen name="Cart" component={CartScreen} options = {{headerShown: false}}/>
+    <Stack.Screen name="Search" component={SearchScreen} options = {{headerShown: false}}/>
+  </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
