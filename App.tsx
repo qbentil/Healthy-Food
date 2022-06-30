5d3eebff5020979f66a5f27c9a175e9746613580
qbentil <bentilshadrack72@gmail.com>
2022-06-30 11:43:05 +0000

    Update App Root

--- App.tsx
+++ App.tsx
@@ -1,23 +1,10 @@
-import { SafeAreaView, Text, View } from 'react-native';
-
-import CartScreen from './screens/Cart';
-import HomeScreen from './screens/Home';
+import BottomTabs from './components/bottomtabs';
 import {NavigationContainer} from '@react-navigation/native';
-import SearchScreen from './screens/Search';
-import StartScreen from './screens/Start';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
   <NavigationContainer>
-    <Stack.Navigator initialRouteName='Start'>
-    <Stack.Screen name="Start" component={StartScreen}  options = {{headerShown: false}}/>
-    <Stack.Screen name="Home" component={HomeScreen} options = {{headerShown: false}}/>
-    <Stack.Screen name="Cart" component={CartScreen} options = {{headerShown: false}}/>
-    <Stack.Screen name="Search" component={SearchScreen} options = {{headerShown: false}}/>
-  </Stack.Navigator>
+    <BottomTabs />
   </NavigationContainer>
   );
 }
