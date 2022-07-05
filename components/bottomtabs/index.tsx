9ec51c6966babee5657a22735ee1e078dcd33c85
qbentil <bentilshadrack72@gmail.com>
2022-07-05 18:16:03 +0000

    Modify Bottom tabs

--- components/bottomtabs/index.tsx
+++ components/bottomtabs/index.tsx
@@ -1,9 +1,6 @@
 import { AntDesign, Ionicons } from '@expo/vector-icons';
+import { Cart, Home, Profile, Search } from '../../screens';
 
-import CartScreen from '../../screens/Cart';
-import HomeScreen from '../../screens/Home';
-import ProfileScreen from '../../screens/Profile';
-import SearchScreen from '../../screens/Search';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Tabs = createBottomTabNavigator();
@@ -17,28 +14,29 @@ const BottomTabs = () => {
                 backgroundColor: '#e91e63'
             },  
             tabBarShowLabel: false,
+            headerShown: false
                         
         }}
         >
-        <Tabs.Screen name="Home" component={HomeScreen} options= {{
+        <Tabs.Screen name="Home" component={Home} options= {{
             tabBarLabel: 'Home',
             tabBarIcon: ({color, size}) => (
                 <AntDesign name="home" color={color} size={size} />
             )
         }} />
-        <Tabs.Screen name="Cart" component={CartScreen} options={{
+        <Tabs.Screen name="Cart" component={Cart} options={{
             tabBarLabel: 'Cart',
             tabBarIcon: ({color, size}) => (
                 <Ionicons name="basket-outline" color={color} size={size} />
             )
         }} />
-        <Tabs.Screen name="Search" component={SearchScreen} options={{
+        <Tabs.Screen name="Search" component={Search} options={{
             tabBarLabel: 'Search',
             tabBarIcon: ({color, size}) => (
                 <AntDesign name="search1" color={color} size={size} />
             )
         }} />
-        <Tabs.Screen name="Profile" component={ProfileScreen} options={{
+        <Tabs.Screen name="Profile" component={Profile} options={{
             tabBarLabel: 'Profile',
             tabBarIcon: ({focused, color, size}: {focused:boolean, color: string, size: number}) => (
                 <AntDesign name="user" color={color} size={size} />
