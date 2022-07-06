f7d4d91b31868644b4b841b890eae6e0e79c25af
qbentil <bentilshadrack72@gmail.com>
2022-07-06 13:02:43 +0000

    Revome Bottom Tab Navs

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
-import { BottomTabs } from './components';
-import {NavigationContainer} from '@react-navigation/native';
+import { Home } from './screens';
+import {NavigationContainer,} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack =   createNativeStackNavigator(); 
 export default function App() {
   return (
   <NavigationContainer>
-    <BottomTabs />
+    <Stack.Navigator screenOptions= {{headerShown: false}} >
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
   </NavigationContainer>
   );
 }
