928eda7bd4c7a313eeb6c89c60976c5cba5f0ee9
qbentil <bentilshadrack72@gmail.com>
2022-07-06 13:03:12 +0000

    Export recently added assets

--- assets/index.tsx
+++ assets/index.tsx
@@ -1,1 +1,9 @@
 export const Logo  =  require("./images/chef1.png");
+export const FilterIcon = require("./images/filter.png");
+
+// filter icons
+export const filterBurgers = require("./images/burgers.png");
+export const filterPizza = require("./images/pizza.png");
+export const filterBarbecue = require("./images/barbecue.png");
+export const filterSushi = require("./images/sushi.png");
+export const filterNoodles = require("./images/noodles.png");
