b21164e1a0a20491add751569cfc8672ccee79c3
qbentil <bentilshadrack72@gmail.com>
2022-07-07 18:07:39 +0000

    Fix Category Item text hitches

--- components/categories/item.tsx
+++ components/categories/item.tsx
@@ -9,7 +9,7 @@ const   CategoryItem = () => {
     <View style={tw`flex items-center justify-center mx-2`}>
         <View style={tw`w-18 h-20 bg-white rounded-lg flex items-center justify-center`}>
           <View style = {tw`w-10 h-10 bg-[#e80013] rounded-full flex items-center justify-center mb-1`}>
-            <MaterialCommunityIcons name="food-turkey" style={tw`text-3xl text-white`} />
+            <MaterialCommunityIcons name="food-turkey" style={tw`text-3xl text-gray-50`} />
           </View>
           <Text style={tw`text-sm`}>Chicken</Text>
         </View>
