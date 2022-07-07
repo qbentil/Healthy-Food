276dbf88a1b79993d201fc3ca10c142e885b0c1c
qbentil <bentilshadrack72@gmail.com>
2022-07-07 16:01:25 +0000

    Create Category Item

---
+++ components/categories/item.tsx
@@ -0,0 +1,19 @@
+import { Image, Text, View } from 'react-native'
+
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import React from 'react'
+import tw from 'twrnc'
+
+const   CategoryItem = () => {
+  return (
+    <View style={tw`flex items-center justify-center`}>
+        <View style={tw`w-18 h-18 bg-white rounded-lg mb-1 flex items-center justify-center`}>
+          <MaterialCommunityIcons name="food-turkey" style={tw`text-4xl`} />
+        </View>
+        <Text>Chicken</Text>
+        
+    </View>
+  )
+}
+
+export default CategoryItem
