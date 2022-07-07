058c1afd0479b19317f10fe940c3f22e1dc9ae50
qbentil <bentilshadrack72@gmail.com>
2022-07-07 15:58:07 +0000

    Implement Context Provider

--- App.tsx
+++ App.tsx
@@ -1,15 +1,19 @@
-import { Home } from './screens';
-import {NavigationContainer,} from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Home } from "./screens";
+import { NavigationContainer } from "@react-navigation/native";
+import { StateProvider } from "./context/StateProvider";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { initialState } from "./context/initialState";
+import reducer from "./context/reducer";
 
-const Stack =   createNativeStackNavigator(); 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-  <NavigationContainer>
-    <Stack.Navigator screenOptions= {{headerShown: false}} >
-      <Stack.Screen name="Home" component={Home} />
-    </Stack.Navigator>
-  </NavigationContainer>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </StateProvider>
   );
 }
-
