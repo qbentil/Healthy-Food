b916c519488fcc4612246d7cf69fbc367eac0299
qbentil <bentilshadrack72@gmail.com>
2022-07-10 00:31:30 +0000

    Render dynamic catgories

--- components/categories/item.tsx
+++ components/categories/item.tsx
@@ -1,6 +1,4 @@
-import { Image, Text, View } from 'react-native'
-
-import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { Text, View } from 'react-native'
 import React from 'react'
 import tw from 'twrnc'
 import { FoodCategory } from '../../types'
@@ -10,9 +8,9 @@ const   CategoryItem = ({category}:{category:FoodCategory}) => {
     <View style={tw`flex items-center justify-center mx-2`}>
         <View style={tw`w-18 h-20 bg-white rounded-lg flex items-center justify-center`}>
           <View style = {tw`w-10 h-10 bg-[#e80013] rounded-full flex items-center justify-center mb-1`}>
-            <MaterialCommunityIcons name="food-turkey" style={tw`text-3xl text-gray-50`} />
+            {category.icon}
           </View>
-          <Text style={tw`text-sm`}>Chicken</Text>
+          <Text style={tw`text-sm`}>{category.name}</Text>
         </View>
         
     </View>
