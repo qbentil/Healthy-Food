6f5c4c1c5ff11540de1ec7c39425f829f0dd9fd3
qbentil <bentilshadrack72@gmail.com>
2022-07-10 00:31:10 +0000

    Styled Icons

--- utils/categories.tsx
+++ utils/categories.tsx
@@ -1,47 +1,47 @@
 import { Entypo, FontAwesome5, MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
-
+import tw from "twrnc";
 const Categories = [
     {
         id: 1,
         name: "Chicken",
         urlParam: 'chicken',
-        icon: <MaterialCommunityIcons name="food-turkey" />,
+        icon: <MaterialCommunityIcons name="food-turkey" style={tw`text-3xl text-gray-50`} />,
     },
     {
         id: 2,
         name: "Fruits",
         urlParam: 'fruits',
-        icon: <MaterialCommunityIcons name="fruit-cherries" />,
+        icon: <MaterialCommunityIcons name="fruit-cherries" style={tw`text-3xl text-gray-50`} />,
     },
     {
         id: 3,
         name: "Soft Drinks",
         urlParam: 'drinks',
-        icon: <Entypo name="drink"  />,
+        icon: <Entypo name="drink"  style={tw`text-3xl text-gray-50`} />,
     },
     {
         id: 4,
         name: "Desserts",
         urlParam: 'desserts',
-        icon: <Entypo name="cake" />,
+        icon: <Entypo name="cake" style={tw`text-3xl text-gray-50`} />,
     },
     {
         id: 5,
         name: "Icecreams",
         urlParam: 'icecreams',
-        icon: <MaterialIcons name="icecream" />,
+        icon: <MaterialIcons name="icecream" style={tw`text-3xl text-gray-50`} />,
     },
     {
         id: 6,
         name: "Fish",
         urlParam: 'fish',
-        icon: <FontAwesome5 name="fish"  />
+        icon: <FontAwesome5 name="fish"  style={tw`text-3xl text-gray-50`} />
     },
     {
         id: 7,
         name: "Rice",
         urlParam: 'rice',
-        icon: <MaterialCommunityIcons name="rice" />,
+        icon: <MaterialCommunityIcons name="rice" style={tw`text-3xl text-gray-50`} />,
     },
     {
         id: 8,
