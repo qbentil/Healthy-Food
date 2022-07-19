342f175f57bf04da01e8d0203cb5e38be6facc58
qbentil <bentilshadrack72@gmail.com>
2022-07-19 16:51:01 +0000

    Update menu section

--- components/sections/menu/index.tsx
+++ components/sections/menu/index.tsx
@@ -1,9 +1,9 @@
-import { Text, View } from 'react-native'
+import { Text, View, ScrollView } from 'react-native'
 
 import React from 'react'
 import SectionHeader from '../header'
 import FoodItem from '../../fooditem'
-
+import tw from 'twrnc'
 interface SectionProps {
     category: string
 }
@@ -11,8 +11,11 @@ const MenuSection = ({category}: SectionProps) => {
   return (
     <View>
       <SectionHeader title={`See our ${category}` } navText={`See more ${category}`} />
-
+      <ScrollView horizontal showsHorizontalScrollIndicator = {false} style={tw``}>
       <FoodItem />
+      <FoodItem />
+      <FoodItem />
+      </ScrollView>
     </View>
   )
 }
