6070e5af6f36a9e429128dd1d6818e0228e1ca0a
qbentil <bentilshadrack72@gmail.com>
2022-07-21 10:21:44 +0000

    Update Menu Section

--- components/sections/menu/index.tsx
+++ components/sections/menu/index.tsx
@@ -1,20 +1,66 @@
-import { Text, View, ScrollView } from 'react-native'
+import { ScrollView, Text, View } from 'react-native'
 
+import FoodItem from '../../fooditem'
 import React from 'react'
 import SectionHeader from '../header'
-import FoodItem from '../../fooditem'
 import tw from 'twrnc'
+
+const Items = [
+  {
+    id: 2,
+    category: 45,
+    name: 'Pizza',
+    imageURI: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    price: '10.00',
+    description: 'Pizza is a savory dish of Italian origin'
+  },
+  {
+    id: 2,
+    category: 45,
+    name: 'Pizza',
+    imageURI: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    price: '10.00',
+    description: 'Pizza is a savory dish of Italian origin'
+  },
+  {
+    id: 2,
+    category: 45,
+    name: 'Pizza',
+    imageURI: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    price: '10.00',
+    description: 'Pizza is a savory dish of Italian origin'
+  },
+  {
+    id: 2,
+    category: 45,
+    name: 'Pizza',
+    imageURI: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    price: '10.00',
+    description: 'Pizza is a savory '
+  },
+  {
+    id: 2,
+    category: 45,
+    name: 'Pizza',
+    imageURI: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    price: '10.00',
+    description: 'Pizza is a savory '
+  },
+  
+]
 interface SectionProps {
     category: string
 }
 const MenuSection = ({category}: SectionProps) => {
   return (
-    <View>
+    <View style={tw`pb-2`}>
       <SectionHeader title={`See our ${category}` } navText={`See more ${category}`} />
-      <ScrollView horizontal showsHorizontalScrollIndicator = {false} style={tw``}>
-      <FoodItem />
-      <FoodItem />
-      <FoodItem />
+      <ScrollView horizontal showsHorizontalScrollIndicator = {false} style={tw`pt-4`}>
+        {
+          Items.map((item, index) => (
+            <FoodItem key={index} item={item} />
+          ))
+        }
       </ScrollView>
     </View>
   )
