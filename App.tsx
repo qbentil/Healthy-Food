5d34f718e55d4314139fcc97dd57739fac426267
qbentil <bentilshadrack72@gmail.com>
2022-11-13 01:57:47 +0000

    Setup Complete, Install twrnc for styling with tailwindcs

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import { StatusBar } from 'expo-status-bar';
+import tw from 'twrnc'
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={[tw`bg-red-500`, styles.container]}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,7 +15,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
