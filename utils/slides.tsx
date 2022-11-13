101efa7cee5c879a5ffe4ed2db1a0865fe396ca3
qbentil <bentilshadrack72@gmail.com>
2022-11-13 14:36:57 +0000

    Create slides data

---
+++ utils/slides.tsx
@@ -0,0 +1,19 @@
+const slides = [
+  {
+    title: "Get discounts on all products",
+    desc: "Save money on all products in our store",
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    title: "Buy premium quality fruits",
+    desc: "We sell only the best quality fruits",
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    title: "Buy Qualiity Diary Products",
+    desc: "We sell only the best quality Diary Products",
+    image: "https://picsum.photos/200/300",
+  },
+];
+
+export default slides;
