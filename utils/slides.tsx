0d26f7e0e0e9f4f1fe9a39d3a91a628cb8b5a74f
qbentil <bentilshadrack72@gmail.com>
2022-11-14 07:13:52 +0000

    Update slides data

--- utils/slides.tsx
+++ utils/slides.tsx
@@ -1,18 +1,27 @@
 const slides = [
   {
+    id: "1",
     title: "Get discounts on all products",
     desc: "Save money on all products in our store",
-    image: "https://picsum.photos/200/300",
+    image: require("../assets/images/splash-1.png"),
   },
   {
+    id: "2",
     title: "Buy premium quality fruits",
     desc: "We sell only the best quality fruits",
-    image: "https://picsum.photos/200/300",
+    image: require("../assets/images/splash-2.png"),
   },
   {
+    id: "3",
     title: "Buy Qualiity Diary Products",
     desc: "We sell only the best quality Diary Products",
-    image: "https://picsum.photos/200/300",
+    image: require("../assets/images/splash-3.png"),
+  },
+  {
+    id: "4",
+    title: "Buy Qualiity Diary Products",
+    desc: "We sell only the best quality Diary Products",
+    image: require("../assets/images/splash-4.png"),
   },
 ];
 
