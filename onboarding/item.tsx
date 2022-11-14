3a17650ffd77882993b426153a0bb60f5d2884a8
qbentil <bentilshadrack72@gmail.com>
2022-11-14 07:15:35 +0000

    Update Onboard Item

--- onboarding/item.tsx
+++ onboarding/item.tsx
@@ -4,10 +4,12 @@ import {
   Text,
   View,
   useWindowDimensions,
+  SafeAreaView,
 } from "react-native";
 
 import { OnboardItem } from "../types";
 import React from "react";
+import tw from "../lib/tw";
 
 interface OnboardItemProps {
   data: OnboardItem;
@@ -17,17 +19,33 @@ const Item = ({ data }: OnboardItemProps) => {
   const { title, desc, image } = data;
   const { width } = useWindowDimensions();
   return (
-    <View style={[styles.container, {width}]}>
-      <Text>{title}</Text>
+    <View style={[tw`flex justify-center relative w-full h-screen`]}>
+      <View style={[tw`h-[60%]`, { width }]}>
+        <Image
+          source={image}
+          style={[styles.image, { width, resizeMode: "contain" }]}
+        />
+      </View>
+      <View style={[tw`h-[40%]`, { width }]}>
+        <View style={[tw`flex justify-center items-center`, { width }]}>
+          <Text style={[tw`text-2xl font-bold`, styles.title]}>{title}</Text>
+          <Text style={[tw`text-base text-center`, styles.desc]}>{desc}</Text>
+        </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
+  image: {
     justifyContent: "center",
+  },
+  title: {
+    color: "#333",
+  },
+  desc: {
+    color: "#333",
+    paddingHorizontal: 64,
   },
 });
 export default Item;
