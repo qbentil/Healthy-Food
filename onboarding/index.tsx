433a7447084dd495cb5c2ffa06fa03cacb2f5aef
qbentil <bentilshadrack72@gmail.com>
2022-11-14 07:15:17 +0000

    Complete slide component

--- onboarding/index.tsx
+++ onboarding/index.tsx
@@ -1,27 +1,44 @@
 import { Animated, FlatList, StyleSheet, Text, View } from "react-native";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 import Item from "./item";
 import slides from "../utils/slides";
 
 const Onboard = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
   const scrollX = useRef(new Animated.Value(0)).current;
+
+  const viewableItemsChanged = useRef(
+    ({ viewableItems }: { viewableItems: any }) => {
+      setCurrentIndex(viewableItems[0].index);
+    }
+  ).current;
+
+  const slidesRef = useRef(null);
+
+  const viewConfig = useRef({ viewAreaCoveragePercentThreshold: 50 }).current;
 
   return (
     <View style={styles.container}>
-      <FlatList
-        data={slides}
-        renderItem={({ item }) => <Item data={item} />}
-        horizontal
-        pagingEnabled
-        showsHorizontalScrollIndicator={false}
-        bounces={false}
-        keyExtractor={(item) => item.title}
-        onScroll={Animated.event(
-          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-          { useNativeDriver: false }
-        )}
-      />
+      <View style={{ flex: 3 }}>
+        <FlatList
+          data={slides}
+          renderItem={({ item }) => <Item data={item} />}
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator
+          bounces={false}
+          keyExtractor={(item) => item.id}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+            { useNativeDriver: false }
+          )}
+          scrollEventThrottle={32}
+          onViewableItemsChanged={viewableItemsChanged}
+          viewabilityConfig={viewConfig}
+          ref={slidesRef}
+        />
+      </View>
     </View>
   );
 };
