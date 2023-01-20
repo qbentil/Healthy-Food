42f7b8d2741af44e4424b655f035485b46f32777
qbentil <bentilshadrack72@gmail.com>
2023-01-20 21:24:51 +0000

    Complete onboarding design

--- screens/onboarding/index.tsx
+++ screens/onboarding/index.tsx
@@ -1,31 +1,31 @@
 import tw from '../../lib/tw'
 import Onboarding from 'react-native-onboarding-swiper';
-import slides from '../../utils/slides';
+import {onboard} from '../../utils/slides';
 import { View, Text, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 const OboardingScreen = ({ navigation }: any) => {
     const DotComponent = ({ selected }: { selected: boolean }) => {
         return (
             <View style={tw`w-4 h-4 flex items-center justify-center rounded-full p-2
-                ${selected ? 'border border-green-500' : ''}
+                ${selected ? 'border border-primary-dark' : ''}
             `}>
                 <View style={tw`w-2 h-2 rounded-full
-                    ${selected ? 'bg-green-500' : 'bg-gray-500'}
+                    ${selected ? 'bg-primary-dark' : 'bg-grey'}
                 `}></View>
             </View>
         )
     }
     const DoneComponent = ({ onPress }: any) => {
         return (
-            <TouchableOpacity onPress={onPress} style={tw`h-8 bg-green-500 rounded-full items-center justify-center flex flex-row px-2 mx-4`}>
+            <TouchableOpacity onPress={onPress} style={tw`h-8 bg-primary-dark rounded-full items-center justify-center flex flex-row px-2 mx-4`}>
                 <Ionicons name="checkmark-done-outline" style={tw`text-xl text-white `} />
-                <Text style={tw`text-white `}>{"Get started"}</Text>
+                <Text style={tw`text-white ml-1`}>{"Go"}</Text>
             </TouchableOpacity>
         )
     }
     const SkipComponent = ({ onPress, skipLabel }: any) => {
         return (
-            <TouchableOpacity onPress={onPress} style={tw`h-8 bg-green-500 rounded-full items-center justify-center flex flex-row px-2 mx-4`}>
+            <TouchableOpacity onPress={onPress} style={tw`h-8 bg-dark rounded-full items-center justify-center flex flex-row px-2 mx-4`}>
                 <Ionicons name="checkmark-done-outline" style={tw`text-xl text-white `} />
                 <Text style={tw`text-white `}>{skipLabel}</Text>
             </TouchableOpacity>
@@ -34,17 +34,18 @@ const OboardingScreen = ({ navigation }: any) => {
     const NextButtonComponent = ({ onPress }: any) => {
         return (
             <TouchableOpacity onPress={onPress} style={tw`h-8 rounded-full items-center justify-center flex flex-row px-2 mx-4`}>
-                <Text style={tw`text-green-500 mr-2`}>Next</Text>
-                <Ionicons name="ios-arrow-redo-outline" style={tw`text-xl text-green-500 `} />
+                <Text style={tw`text-primary mr-2`}>Next</Text>
+                <Ionicons name="ios-arrow-redo-outline" style={tw`text-xl text-primary `} />
             </TouchableOpacity>
         )
     }
     return (
         <Onboarding
-            pages={slides}
+            pages={onboard}
             nextLabel={'Next'}
             onSkip={() => navigation.navigate('HomeScreen')}
             onDone={() => navigation.navigate('HomeScreen')}
+            skipLabel={'Skip'}
             DotComponent={DotComponent}
             bottomBarHighlight={false}
             transitionAnimationDuration={1}
