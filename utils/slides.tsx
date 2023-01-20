6549106c1d41f9b672de294179030f10be8424e9
qbentil <bentilshadrack72@gmail.com>
2023-01-20 17:22:55 +0000

    Create onboard slides object

--- utils/slides.tsx
+++ utils/slides.tsx
@@ -6,25 +6,25 @@ const slides = [
     backgroundColor: "#fff",
     title: "Get discounts on all products",
     subtitle: "Save money on all products in our store",
-    image: <Image source={require("../assets/images/splash-1.png")} style={tw`w-84 h-84 rounded-full object-contain`} />,
+    image: <Image source={require("../assets/images/splash-1.png")} style={tw`w-84 h-84 rounded-full`} />,
   },
   {
     backgroundColor: "#fff",
     title: "Buy premium quality fruits",
     subtitle: "We sell only the best quality fruits",
-    image: <Image source={require("../assets/images/splash-2.png")} style={tw`w-84 h-84 rounded-full object-contain`} />,
+    image: <Image source={require("../assets/images/splash-2.png")} style={tw`w-84 h-84 rounded-full`} />,
   },
   {
     backgroundColor: "#fff",
     title: "Buy Qualiity Diary Products",
     subtitle: "We sell only the best quality Diary Products",
-    image: <Image source={require("../assets/images/splash-3.png")} style={tw`w-84 h-84 rounded-full object-contain`} />,
+    image: <Image source={require("../assets/images/splash-3.png")} style={tw`w-84 h-84 rounded-full`} />,
   },
   {
     backgroundColor: "#fff",
     title: "Buy Qualiity Diary Products",
     subtitle: "We sell only the best quality Diary Products",
-    image: <Image source={require("../assets/images/splash-4.png")} style={tw`w-84 h-84 rounded-full object-contain`} />,
+    image: <Image source={require("../assets/images/splash-4.png")} style={tw`w-84 h-84 rounded-full`} />,
   },
 ];
 
