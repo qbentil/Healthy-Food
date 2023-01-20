eb673d2bf5f16f639009b0b56223f6cc15bed65d
qbentil <bentilshadrack72@gmail.com>
2023-01-20 00:48:06 +0000

    Remove dummy onboarding compoennt

--- components/onboarding/index.tsx
+++ components/onboarding/index.tsx
@@ -3,52 +3,15 @@ import React, { useRef, useState } from "react";
 
 import Item from "./item";
 import slides from "../../utils/slides";
+import tw from "../../lib/tw";
 
 const Onboard = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const scrollX = useRef(new Animated.Value(0)).current;
 
-  const viewableItemsChanged = useRef(
-    ({ viewableItems }: { viewableItems: any }) => {
-      setCurrentIndex(viewableItems[0].index);
-    }
-  ).current;
-
-  const slidesRef = useRef(null);
-
-  const viewConfig = useRef({ viewAreaCoveragePercentThreshold: 50 }).current;
 
   return (
-    <View style={styles.container}>
-      <View style={{ flex: 3 }}>
-        <FlatList
-          data={slides}
-          renderItem={({ item }) => <Item data={item} />}
-          horizontal
-          pagingEnabled
-          showsHorizontalScrollIndicator
-          bounces={false}
-          keyExtractor={(item) => item.id}
-          onScroll={Animated.event(
-            [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-            { useNativeDriver: false }
-          )}
-          scrollEventThrottle={32}
-          onViewableItemsChanged={viewableItemsChanged}
-          viewabilityConfig={viewConfig}
-          ref={slidesRef}
-        />
-      </View>
+    <View style={tw``}>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
 export default Onboard;
