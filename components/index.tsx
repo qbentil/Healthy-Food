2d525b0c29951abcfa3845ce3ba5cd9000ac1fc8
qbentil <bentilshadrack72@gmail.com>
2023-01-21 11:19:10 +0000

    Export newly created components

---
+++ components/index.tsx
@@ -0,0 +1,4 @@
+export {default as Button} from './Button'
+export {default as Header} from "./Header"
+export {default as LoginForm} from './loginform' 
+export {default as SignupForm} from './signupform'
