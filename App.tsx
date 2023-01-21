8a8e978c6a4bf72d8aaba4d885850531812089c7
qbentil <bentilshadrack72@gmail.com>
2023-01-21 11:16:55 +0000

    Add New Screens to Stack

--- App.tsx
+++ App.tsx
@@ -1,15 +1,20 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { HomeScreen, OnboardingScreen , SplashScreen} from './screens';
+import { HomeScreen, LoginScreen, OnboardingScreen , SplashScreen,SignupScreen, WelcomeScreen} from './screens';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="SplashScreen" component={SplashScreen} options={{headerShown: false,}} />
+        {/* <Stack.Screen name="SplashScreen" component={SplashScreen} options={{headerShown: false,}} /> */}
         <Stack.Screen name="OnboardingScreen" component={OnboardingScreen} options={{headerShown: false,}} />
+        <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} options={{headerShown: false,}} />
         <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown: false,}} />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} options={{headerShown: false,}} />
+        <Stack.Screen name="SignupScreen" component={SignupScreen} options={{headerShown: false,}} />
+
+
       </Stack.Navigator>
     </NavigationContainer>
   );
