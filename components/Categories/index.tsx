41cca04a7c107a5a0293879f6733b9e24bd43cb9
qbentil <bentilshadrack72@gmail.com>
2023-01-22 00:02:12 +0000

    Create categories component

---
+++ components/Categories/index.tsx
@@ -0,0 +1,25 @@
+import { View, Text, FlatList } from 'react-native'
+import React from 'react'
+import SectionHeader from '../sectionheader'
+import tw from '../../lib/tw'
+import Item from './items'
+import { CategoriesData } from '../../utils'
+const Categories = () => {
+  return (
+    <View style={tw`w-[95%]`}>
+      <SectionHeader title="Categories" />
+      {/* horizontal flatlist of categories*/}
+      <View style={tw`my-2`}>
+        <FlatList
+          data={CategoriesData}
+          renderItem={({ item }) => <Item data={item} />}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={item => item.id}
+        />
+      </View>
+    </View>
+  )
+}
+
+export default Categories
