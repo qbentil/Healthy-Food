2491e77711f0a909da1809fbe4dc00d2a2e9dc81
qbentil <bentilshadrack72@gmail.com>
2023-01-22 00:17:25 +0000

    Export newly created compone

--- components/index.tsx
+++ components/index.tsx
@@ -2,3 +2,7 @@ export {default as Button} from './Button'
 export {default as Header} from "./Header"
 export {default as LoginForm} from './loginform' 
 export {default as SignupForm} from './signupform'
+export {default as Searchbar} from './searchbar'
+export {default as Showcase} from './Showcase'
+export {default as Categories} from './Categories'
+export {default as Products} from './Products'
