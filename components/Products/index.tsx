a8e985be9a1ea3991be0c44b91d187b6d6e69ee9
qbentil <bentilshadrack72@gmail.com>
2023-01-23 12:46:46 +0000

    Render category FlatList in 2 columns

--- components/Products/index.tsx
+++ components/Products/index.tsx
@@ -1,12 +1,23 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
 import tw from '../../lib/tw'
 import SectionHeader from '../sectionheader'
+import { ProductsData } from '../../utils'
+import Item from './item'
 const Products = () => {
   return (
     <View style={tw`w-[95%]`}>
         <SectionHeader title="Featured products" />
-      <Text>Products</Text>
+        {/* create flatlist with 2 items on a row */}
+        <View style={tw`flex`}>
+        <FlatList
+            data={ProductsData}
+            renderItem={({item}) => <Item data={item} />}
+            key={'#'}
+            keyExtractor={item => "#" + item.id}         
+            numColumns={2}
+        />
+        </View>
     </View>
   )
 }
