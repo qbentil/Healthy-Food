9f59485801f8ce5bc41ddc0acade33682713be5b
qbentil <bentilshadrack72@gmail.com>
2023-01-24 11:02:58 +0000

    Component flatlist fix

--- components/Products/index.tsx
+++ components/Products/index.tsx
@@ -1,23 +1,27 @@
-import { View, Text, FlatList } from 'react-native'
-import React from 'react'
-import tw from '../../lib/tw'
-import SectionHeader from '../sectionheader'
-import { ProductsData } from '../../utils'
+import { FlatList, Text, View } from 'react-native'
+
 import Item from './item'
+import { ProductsData } from '../../utils'
+import React from 'react'
+import SectionHeader from '../sectionheader'
+import tw from '../../lib/tw'
+
 const Products = () => {
   return (
-    <View style={tw`w-[95%]`}>
-        <SectionHeader title="Featured products" />
-        {/* create flatlist with 2 items on a row */}
-        <View style={tw`flex`}>
+    <View style={[tw`w-[95%]`, { flex: 1 }]}>
+      <SectionHeader title="Featured products" />
+      {/* create flatlist with 2 items on a row */}
+      <View style={[tw`h-full `, { flex: 1 }]}>
         <FlatList
-            data={ProductsData}
-            renderItem={({item}) => <Item data={item} />}
-            key={'#'}
-            keyExtractor={item => "#" + item.id}         
-            numColumns={2}
+          data={ProductsData}
+          renderItem={({ item }) => <Item data={item} />}
+          key={'#'}
+          keyExtractor={item => "#" + item.id}
+          numColumns={2}
+          columnWrapperStyle={tw`justify-between`}
+          showsVerticalScrollIndicator={false}
         />
-        </View>
+      </View>
     </View>
   )
 }
