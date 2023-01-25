e2621030728ddeeb6261599e2b4edc5f85407289
qbentil <bentilshadrack72@gmail.com>
2023-01-25 14:07:07 +0000

    Update section headers

--- components/sectionheader/index.tsx
+++ components/sectionheader/index.tsx
@@ -1,14 +1,16 @@
-import { View, Text , TouchableOpacity} from 'react-native'
-import React from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
+
 import { Entypo } from '@expo/vector-icons';
+import React from 'react'
 import tw from '../../lib/tw'
+
 interface Props {
     title: String,
     onPress?: () => void
 }
 const SectionHeader = ({title, onPress}:Props) => {
   return (
-    <View style={tw`w-full flex flex-row items-center justify-between text-dark mb-2`}>
+    <View style={tw`w-full bg-white flex flex-row items-center justify-between text-dark mb-2`}>
       <Text style={tw`text-xl font-bold `}>{title}</Text>
       <TouchableOpacity onPress={onPress}>
         <Entypo name="chevron-right" style={tw`text-2xl text-primary-dark`} />
