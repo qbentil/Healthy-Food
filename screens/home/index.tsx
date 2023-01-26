d79ee770bcf1940b061ce9598037cebaec330352
qbentil <bentilshadrack72@gmail.com>
2023-01-26 16:13:18 +0000

    Update the home

--- screens/home/index.tsx
+++ screens/home/index.tsx
@@ -1,11 +1,11 @@
 import { Categories, Products, Searchbar, Showcase, } from '../../components'
 import { SafeAreaView, ScrollView, Text, View } from 'react-native'
-
+import { BottomTabs } from '../../components'
 import tw from '../../lib/tw'
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView style={[tw`bg-bg-a w-full h-full flex items-center justify-center`, { flex: 1 }]}>
+    <SafeAreaView style={[tw`bg-white w-full h-[120%] flex items-center justify-center`, { flex: 1 }]}>
       <Searchbar />
       <ScrollView style={[tw`w-full`, { flex: 1 }]}
         contentContainerStyle={[tw`flex items-center justify-center`]}
@@ -15,11 +15,7 @@ const HomeScreen = () => {
         <Categories />
         <Products />
       </ScrollView>
-      <View style={[tw`w-full h-20 bg-white`, { flex: 0.1 }]}>
-        <Text>
-          Bottom Tab Navigator here
-        </Text>
-      </View>
+      <BottomTabs />
     </SafeAreaView>
   )
 }
